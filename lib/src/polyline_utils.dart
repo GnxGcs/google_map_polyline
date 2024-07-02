05634b691b4456a305e1b1e7b693746c5d17254a
BENKO Claudia <cbenko@generixgroup.com>
2024-07-02 12:50:35 +0300

    compile issue fix

--- lib/src/polyline_utils.dart
+++ lib/src/polyline_utils.dart
@@ -43,69 +43,70 @@ class PolylineUtils {
         'X-Ios-Bundle-Identifier': _data!.xIosBundleIdentifier,
       });
 
-    _response = await _dio.get(
-        "https://maps.googleapis.com/maps/api/directions/json",
-        options: _options,
-        queryParameters: qParam);
+      _response = await _dio.get(
+          "https://maps.googleapis.com/maps/api/directions/json",
+          options: _options,
+          queryParameters: qParam);
 
-    try {
-      if (_response.statusCode == 200) {
-        if (_response.data["status"] == "REQUEST_DENIED") {
-          throw Exception(_response.data["error_message"]);
+      try {
+        if (_response.statusCode == 200) {
+          if (_response.data["status"] == "REQUEST_DENIED") {
+            throw Exception(_response.data["error_message"]);
+          }
+
+          _coordinates = decodeEncodedPolyline(
+              _response.data['routes'][0]['overview_polyline']['points']);
         }
-
-        _coordinates = decodeEncodedPolyline(
-            _response.data['routes'][0]['overview_polyline']['points']);
+      } catch (e) {
+        print('[ERROR_POLYLINE] ${e.toString()}');
       }
-    } catch (e) {
-      print('[ERROR_POLYLINE] ${e.toString()}');
+
+      return _coordinates;
     }
 
-    return _coordinates;
-  }
+    List<LatLng> decodeEncodedPolyline(String encoded) {
+      List<LatLng> poly = [];
+      int index = 0, len = encoded.length;
+      int lat = 0, lng = 0;
 
-  List<LatLng> decodeEncodedPolyline(String encoded) {
-    List<LatLng> poly = [];
-    int index = 0, len = encoded.length;
-    int lat = 0, lng = 0;
+      while (index < len) {
+        int b, shift = 0, result = 0;
+        do {
+          b = encoded.codeUnitAt(index++) - 63;
+          result |= (b & 0x1f) << shift;
+          shift += 5;
+        } while (b >= 0x20);
+        int dlat = ((result & 1) != 0 ? ~(result >> 1) : (result >> 1));
+        lat += dlat;
 
-    while (index < len) {
-      int b, shift = 0, result = 0;
-      do {
-        b = encoded.codeUnitAt(index++) - 63;
-        result |= (b & 0x1f) << shift;
-        shift += 5;
-      } while (b >= 0x20);
-      int dlat = ((result & 1) != 0 ? ~(result >> 1) : (result >> 1));
-      lat += dlat;
-
-      shift = 0;
-      result = 0;
-      do {
-        b = encoded.codeUnitAt(index++) - 63;
-        result |= (b & 0x1f) << shift;
-        shift += 5;
-      } while (b >= 0x20);
-      int dlng = ((result & 1) != 0 ? ~(result >> 1) : (result >> 1));
-      lng += dlng;
-      LatLng p = new LatLng((lat / 1E5).toDouble(), (lng / 1E5).toDouble());
-      poly.add(p);
+        shift = 0;
+        result = 0;
+        do {
+          b = encoded.codeUnitAt(index++) - 63;
+          result |= (b & 0x1f) << shift;
+          shift += 5;
+        } while (b >= 0x20);
+        int dlng = ((result & 1) != 0 ? ~(result >> 1) : (result >> 1));
+        lng += dlng;
+        LatLng p = new LatLng((lat / 1E5).toDouble(), (lng / 1E5).toDouble());
+        poly.add(p);
+      }
+      return poly;
     }
-    return poly;
-  }
 
-  String? getMode(RouteMode? _mode) {
-    switch (_mode) {
-      case RouteMode.driving:
-        return 'driving';
-      case RouteMode.walking:
-        return 'walking';
-      case RouteMode.bicycling:
-        return 'bicycling';
-      case RouteMode.transit:
-        return 'transit';
-      default:
-        return null;
+    String? getMode(RouteMode? _mode) {
+      switch (_mode) {
+        case RouteMode.driving:
+          return 'driving';
+        case RouteMode.walking:
+          return 'walking';
+        case RouteMode.bicycling:
+          return 'bicycling';
+        case RouteMode.transit:
+          return 'transit';
+        default:
+          return null;
+      }
     }
   }
 }
